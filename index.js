c911259eaca993c24660dcfc11d556d93b343c22
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-07-30 15:07:44 +0600

    make project details end-point

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://ashrafulislam:${process.env.db_pass}@cluster0.gqju11e.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,39 +20,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    await client.connect();
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const projectsCollection = client.db("ashrafulislamDB").collection("projects");
 
+    // Create an index on the projectTitle field
+    await projectsCollection.createIndex({ projectTitle: 1 });
 
-    app.get("/projects", async(req, res) => {
-        const cursor = projectsCollection.find().sort({_id: -1});
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/projects", async (req, res) => {
+      const cursor = projectsCollection.find().sort({ _id: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post("/projects", async(req, res) => {
-        const project = req.body;
-        const result = await projectsCollection.insertOne(project);
-        res.send(result);
-        console.log(result);
-    })
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get("/project/:title", async (req, res) => {
+      try {
+        const title = req.params.title;
+        const query = { projectTitle: title }; // Use title directly as a string
+        const result = await projectsCollection.findOne(query);
+        if (result) {
+          res.status(200).json(result);
+        } else {
+          res.status(404).send("Project not found");
+        }
+      } catch (error) {
+        console.error("Error fetching project:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.post("/projects", async (req, res) => {
+      const project = req.body;
+      const result = await projectsCollection.insertOne(project);
+      res.send(result);
+      console.log(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
+    // Uncomment the following line if you want to close the client after the operations
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('server is running');
-})
+  res.send('Server is running');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
